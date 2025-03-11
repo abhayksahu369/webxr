6b3e12efc37a53e1cf4b9b03831c4f8344a161b8
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2025-03-11 22:08:26 +0530

    aa

--- src/CubeContainer.jsx
+++ src/CubeContainer.jsx
@@ -1,11 +1,14 @@
 import {Canvas} from '@react-three/fiber'
 import {OrbitControls} from "@react-three/drei"
-import {XR,XRHitTest,createXRStore} from "@react-three/xr"
+import {XR,XRHitTest,createXRStore,XRDomOverlay} from "@react-three/xr"
 import Model from './Model'
+import { useState } from 'react'
+
 
 const store = createXRStore()
 
 export default function CubeContainer() {
+  const [message, setMessage] = useState("Welcome to WebXR!");
   return (
     <>
     <button onClick={() => store.enterAR()}>Enter AR</button>
@@ -16,6 +19,15 @@ export default function CubeContainer() {
         <directionalLight position={[5, 5, 5]} intensity={2} />
         <directionalLight position={[-5, -5, 5]} intensity={1.5} />
         <pointLight position={[0, 2, 2]} intensity={1.5} />
+        <XRDomOverlay
+            style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          >
+            <div
+              style={{ backgroundColor: 'green', padding: '1rem 2rem' }}
+            >
+ Hello World
+            </div>
+          </XRDomOverlay>
 
         <XRHitTest>
             {/* <boxGeometry args={[2,2,2]}/> */}
