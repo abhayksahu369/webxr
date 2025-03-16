ff1dd7441365fe4f0b60391e99b76c86f817c81e
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2025-03-16 12:29:53 +0530

    testing dashboard

--- src/DashboardInAR.jsx
+++ src/DashboardInAR.jsx
@@ -6,14 +6,16 @@ const DashboardInAR = () => {
   const [visible, setVisible] = useState(true);
 
   return (
-    <mesh position={[0, 1.5, -2]} scale={[1, 1, 1]}>
+    <mesh position={[0, 0.5, -0.5]} scale={[0.5, 0.5, 0.5]}>
       {/* 3D Plane for Dashboard */}
-      <planeGeometry args={[1.5, 1]} />
-      <meshStandardMaterial color="white" transparent opacity={0.5} />
+    
 
       {/* Render HTML inside AR */}
       <Html transform position={[0, 0, 0.01]}>
-        <Dashboard/>
+        <div >
+        hellooooo
+        {/* <Dashboard/> */}
+        </div>
       </Html>
     </mesh>
   );
